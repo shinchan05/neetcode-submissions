74a398c1012eb021985ceaabed6d97d6bd169f54
shinchan05 <133595684+shinchan05@users.noreply.github.com>
2026-04-16 19:57:26 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.cpp
@@ -0,0 +1,29 @@
+#include<string.h>
+class Solution {
+public:
+    bool isAnagram(string s, string t) 
+    {
+        if(s.length()!=t.length())
+        {
+            return false;
+        }
+       int arr[26]={0};
+       for(int i=0;i<s.length();i++)
+       {
+          arr[s[i]-'a']++;
+       }
+       for(int i=0;i<t.length();i++)
+       {
+          arr[t[i]-'a']--;
+       }
+       for(int i=0;i<26;i++)
+       {
+          if(arr[i]!=0)
+          {
+            return false;
+          }
+       }
+       return true;
+
+    }
+};
